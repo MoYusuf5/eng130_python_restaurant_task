1775cebf1495b3ba93ee384c614c16dcb207d600
Mohamed <mohamedyusuf45@hotmail.com>
2022-10-12 17:51:48 +0100

    task added

--- restaurant_task.py
+++ restaurant_task.py
@@ -8,3 +8,25 @@
 #3
 # As a user, I want to have my order read back to me in formated way so I know what I ordered.
 
+menu = {
+    "starters" : ["spring rolls", "Soup", "Salad"],
+    "mains" : ["grilled beef", "fried fish"],
+    "desserts" : ["apple crumble", "ice cream", "cheesecake"]
+}
+
+print("Do You Want To See Our Menu? yes/no: ")
+view_menu = str(input())
+
+if view_menu == "yes":
+    print(menu)
+    view_menu = str(input("What would you like to order?"))
+
+
+elif view_menu == "no":
+    print("Thanks, Enjoy Your Day!")
+
+
+
+
+
+
